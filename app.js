720615beb45d04b88125d9919a02b21ebd1ebfdd
Joel Shooster <JBShooster@gmail.com>
2016-03-30 16:33:02 -0700

    Update

--- app.js
+++ app.js
@@ -3,22 +3,36 @@ findScore = function(score){
   score = score.replace(/\-/g,"");
   score = score.split("")
 
-  if(score[score.length-3] === "X"){
-    score.splice(-2, 2)
-  }
+   //if(score[score.length-3] === "X"){
+   //  score.splice(-2, 2)
+   //}
+
 
   if(score[score.length-2] === "/"){
     score.splice(-1, 1)
   }
 
   for (var i = 0; i < score.length; i++) {
-    if(score[i] === "X"){
-      score[i] = 30;
+    if(score[i] === "X" && score[i+1] === "X" && score[i+2] === "X"){
+      score[i] = 30 ;
+    }
+    else if(score[i] === "X" && score[i+1] !== "X" && score[i] === "/"){
+      score[i] = 20 + parseInt(score[i+1]);
+    }
+    else if(score[i] === "X" && score[i+1] === "X" && score[i] !== "X"){
+      score[i] = 20 + parseInt(score[i+2]);
+    }
+    else if(score[i] === "X" && score[i+2] !== "/"){
+      score[i] = 10 + parseInt(score[i+1]);
     }
     else if(score[i] === "/"){
       score[i-1] = parseInt(score[i-1]) ;
       score[i-1] += 10;
       delete score[i];
+    }
+    else if(score[score.length-2] === "X"){
+      score[score.length-3] = 30;
+      score.splice(-2,2);
     }
     else{
       score[i] = parseInt(score[i]);
@@ -39,3 +53,7 @@ findScore("XXXXXXXXXXXX");
 
 //Expect to return 150
 findScore("5/5/5/5/5/5/5/5/5/5/5");
+
+//Expect to return 100
+findScore("X9-9-9-9-9-9-9-9-9-");
+
