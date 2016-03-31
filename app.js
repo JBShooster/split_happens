a0bbd45f70aba531bf43863c4ff388baa1554236
Joel Shooster <JBShooster@gmail.com>
2016-03-31 10:25:56 -0700

    New solution that passes 'X9-9-9-9-9-9-9-9-9-'

--- app.js
+++ app.js
@@ -1,43 +1,30 @@
 //Split Happens
 findScore = function(score){
-  score = score.replace(/\-/g,"");
+  score = score.replace(/\-/g,0);
   score = score.split("")
 
-   //if(score[score.length-3] === "X"){
-   //  score.splice(-2, 2)
-   //}
+   if(score[score.length-2] === "/"){
+     score.splice(-1, 1)
+   }
 
-
-  if(score[score.length-2] === "/"){
-    score.splice(-1, 1)
-  }
-
-  for (var i = 0; i < score.length; i++) {
-    if(score[i] === "X" && score[i+1] === "X" && score[i+2] === "X"){
-      score[i] = 30 ;
+   for(var i=0; i<score.length; i++){
+    if(score[i] === "/"){
+      score[i] = 10;
     }
-    else if(score[i] === "X" && score[i+1] !== "X" && score[i] === "/"){
-      score[i] = 20 + parseInt(score[i+1]);
+    else if(score[i] === "X" && score[i+1] === "X" && score[i+2] === "X"){
+      score[i] = 30;
     }
-    else if(score[i] === "X" && score[i+1] === "X" && score[i] !== "X"){
-      score[i] = 20 + parseInt(score[i+2]);
+    else if(score[i] === "X" && score[i+1] !== "X"){
+      score[i] = 10 + parseInt(score[i+1]) + parseInt(score[i+2]);
     }
-    else if(score[i] === "X" && score[i+2] !== "/"){
-      score[i] = 10 + parseInt(score[i+1]);
-    }
-    else if(score[i] === "/"){
-      score[i-1] = parseInt(score[i-1]) ;
-      score[i-1] += 10;
-      delete score[i];
-    }
-    else if(score[score.length-2] === "X"){
-      score[score.length-3] = 30;
-      score.splice(-2,2);
+    else if(score[score.length-1] === "X" || score[score.length-2] === "X"){
+      score[score.length-3] === 30;
+      score.splice(-2, 2);
     }
     else{
       score[i] = parseInt(score[i]);
     }
-  }
+   }
 
   console.log("Score Table: " + score);
   console.log(score.reduce(function(a,b){
